7d11eb88fdd414813dbe5e0be3f680c8100dbd57
Natan Orzechowski <natan.orzechowski@nask.pl>
2021-11-09 17:44:33 +0100

    Add settings_getter and logger_initializator

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+from loguru import logger
+from src.logger_initializator import initialize_logger
+
+@logger.catch
 def main():
     pass
+    logger.info("Test")
+
 
 
 if __name__ == "__main__":
+    initialize_logger()
     main()
---
+++ src/logger_initializator.py
@@ -0,0 +1,28 @@
+import sys
+from loguru import logger
+from datetime import date, datetime
+from src.settings_getter import get_settings_in_dict
+
+
+def initialize_logger():
+    LOG_FILE_EXTENSION = ".log"
+    SETTINGS = get_settings_in_dict()
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        format="{time} {level} {message}",
+        filter="my_module",
+        level=SETTINGS["LOG_LEVEL"],
+    )
+    day = date.today()
+    now = datetime.now()
+    log_filename = (now.strftime("%H:%M:%S")).replace(":", "") + day.strftime("%b-%d-%Y") + LOG_FILE_EXTENSION
+    logger.add(SETTINGS["LOG_DIRECTORY"] + log_filename)
+
+
+def main():
+    raise NotImplementedError("Use as package")
+
+
+if __name__ == "__main__":
+    main()
