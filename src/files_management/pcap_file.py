11f9403cb1a6b887696df4e936216b4717d6742f
TrywialnyOrzech <natan.orze@gmail.com>
2021-12-19 19:26:24 +0100

    Implement pcap and txt file read

--- src/files_management/pcap_file.py
+++ src/files_management/pcap_file.py
@@ -1,8 +1,7 @@
-from loguru import logger
+import inspect
 import pyshark
+from loguru import logger
 
-
-from src.files_management.files_search import search_valid_files_recursively
 from src.files_management.file import File
 
 
@@ -10,13 +9,17 @@ class PCAP_File(File):
     def __init__(cls, path) -> None:
         super().__init__()
         cls.EXTENSION = ".pcap"
-        cls.__path = path
+        cls.path = path
 
     def read(cls) -> list:
-        pcap = pyshark.FileCapture(cls.__path)
-        packet_list = [packet for packet in pcap]
-        return packet_list
-
+        try:
+            if cls.check_extension(cls.EXTENSION, cls.path) == False:
+                raise ValueError(f"{inspect.currentframe().f_code.co_name}: invalid file extension. Expects {cls.EXTENSION}, got {cls.path} instead")
+            pcap = pyshark.FileCapture(cls.path)
+            packet_list = [packet for packet in pcap]
+            return packet_list
+        except Exception as e:
+            logger.error(str(e))
 
 def main():
     raise NotImplementedError("Use as package")
--- src/files_management/txt_file.py
+++ src/files_management/txt_file.py
@@ -1,6 +1,6 @@
+import inspect
 from loguru import logger
 
-from src.files_management.files_search import search_valid_files_recursively
 from src.files_management.files import File
 
 
@@ -8,13 +8,17 @@ class TXT_File(File):
     def __init__(cls, path) -> None:
         super().__init__()
         cls.EXTENSION = ".txt"
-        cls.__path = path
+        cls.path = path
 
     def read(cls) -> list:
-        with open(cls.__path) as f:
-            txt_line_by_line = f.readlines()
-        return txt_line_by_line
-
+        try:
+            if cls.check_extension(cls.EXTENSION, cls.path) == False:
+                raise ValueError(f"{inspect.currentframe().f_code.co_name}: invalid file extension. Expects {cls.EXTENSION}, got {path} instead")
+            with open(cls.path) as f:
+                txt_line_by_line = f.readlines()
+            return txt_line_by_line
+        except Exception as e:
+            logger.error(str(e))
 
 def main():
     raise NotImplementedError("Use as package")
